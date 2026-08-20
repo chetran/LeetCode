8423655d01acb0c58dfc3978784b5f6444729f7a
chetran <chelongtran@gmail.com>
2026-08-20 08:00:31 +0200

    refactor: split utils and solution

---
+++ LinkedList/ll_utils.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+def checker(received, answer):
+    if received != answer:
+        print(f'FAILED: Received: {received}, but expected: {answer}.')
+        return 
+    print("PASSED!")
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def to_linked_list(arr: list) -> Optional[ListNode]:
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    curr = head
+    for val in arr[1:]:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return head
+
+def to_python_list(head: Optional[ListNode]) -> list:
+    arr = []
+    curr = head
+    while curr:
+        arr.append(curr.val)
+        curr = curr.next
+    print(arr)
+    return arr
--- LinkedList/reverseList.py
+++ LinkedList/reverseList.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
+from ll_utils import to_python_list, to_linked_list, ListNode
 from typing import Optional
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -15,25 +10,6 @@ class Solution:
         while res:
             res.next, prev, res = prev, res, res.next
         return prev
-
-def to_linked_list(arr: list) -> Optional[ListNode]:
-    if not arr:
-        return None
-    head = ListNode(arr[0])
-    curr = head
-    for val in arr[1:]:
-        curr.next = ListNode(val)
-        curr = curr.next
-    return head
-
-def to_python_list(head: Optional[ListNode]) -> list:
-    arr = []
-    curr = head
-    while curr:
-        arr.append(curr.val)
-        curr = curr.next
-    print(arr)
-    return arr
 
 if __name__ == '__main__':
     solution = Solution()
